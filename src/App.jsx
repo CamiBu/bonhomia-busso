0883738cdba3268cc5e76a9f97a1969b380d2d68
Cami Busso <c.busso@live.com.ar>
2021-06-14 21:48:06 -0300

    navbar

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import  NavbarComponent from './component/navBar';
+
+
+function App() {
+  return (
+    <>
+    <NavbarComponent/> 
+    </>  
+  );
+}
+
+export default App;
